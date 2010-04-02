4558858d9cf02afedae5ea6670e03e49dccdd87a
root <root@ubuntu.(none)>
2010-04-02 08:05:45 -0700

    Added pagination to xprof Runs table

---
+++ xhprof.js
@@ -0,0 +1,15 @@
+if (Drupal.jsEnabled) {
+  $(function() {
+    /*
+    $("table#xhprof-runs-table tr").bind('mouseenter', function() {
+      $tr = $(this);
+      $trOptions = $('<tr colspan="4"><td>Options</td></tr>');
+      $tr.after($trOptions);
+    });
+
+    $("table#xhprof-runs-table tr").bind('mouseleave', function() {
+      $(this).next().remove();
+    });
+    */
+  });
+}
