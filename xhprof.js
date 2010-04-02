0b1a9af523a93841935e81540bcac8c6e9dc0aa4
root <root@ubuntu.(none)>
2010-04-02 13:46:55 -0700

    Added more data to the xhprof runs table.  Added a 'clear' and 'refresh' button to the xhprof runs table.

--- xhprof.js
+++ xhprof.js
@@ -1,15 +1,9 @@
 if (Drupal.jsEnabled) {
   $(function() {
-    /*
-    $("table#xhprof-runs-table tr").bind('mouseenter', function() {
-      $tr = $(this);
-      $trOptions = $('<tr colspan="4"><td>Options</td></tr>');
-      $tr.after($trOptions);
+    $('#xhprof-clear-runs').click(function() {
+      if (!confirm(Drupal.t('Are you sure you want to delete all of your xhprof run data?'))) {
+        return false;
+      }
     });
-
-    $("table#xhprof-runs-table tr").bind('mouseleave', function() {
-      $(this).next().remove();
-    });
-    */
   });
 }
