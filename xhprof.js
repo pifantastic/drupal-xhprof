21aaf8176d402c150fd7a43e4cf4f3dd1f45afa6
pifantastic <aaron.forsander@gmail.com>
2010-06-28 23:57:36 -0700

    Added hook_perm and some permissions checks.  Now using a behavior in the js file.  Also replaced some occurences of 'xhprof' with 'XHProf'

--- xhprof.js
+++ xhprof.js
@@ -1,9 +1,7 @@
-if (Drupal.jsEnabled) {
-  $(function() {
-    $('#xhprof-clear-runs').click(function() {
-      if (!confirm(Drupal.t('Are you sure you want to delete all of your xhprof run data?'))) {
-        return false;
-      }
-    });
+Drupal.behaviors.xhprof = function() {
+  $('#xhprof-clear-runs').click(function() {
+    if (!confirm(Drupal.t('Are you sure you want to delete all of your XHProf run data?'))) {
+      return false;
+    }
   });
-}
+};
